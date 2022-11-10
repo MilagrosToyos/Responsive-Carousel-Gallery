985f80dee67fb365fb9e120e79b148c2039bf0cd
Milagros Toyos <95191805+MilagrosToyos@users.noreply.github.com>
2022-11-09 22:40:48 -0300

    correciones dropdown

--- index.js
+++ index.js
@@ -45,21 +45,7 @@ function myFunction() {
     }
   }
 
-/*VER COMO ARREGLAR ESTO PARA QUE FUNCIONE EL DROPDOWN DEL NAVBAR*/
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function dropdwn() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  
- }
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(e) {
-    if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
-    }
-  }
+
+
 
 
